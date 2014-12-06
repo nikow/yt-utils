67e6d100e89ed6da362cd333b20e8aa6ccf514cc
Mariusz Klinikowski <nikowek@gmail.com>
2014-12-06 13:17:41 +0100

    Added first version of yt.sh

---
+++ yt.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#set -x
+#set -e
+
+if [ -z $@ ]; then
+    echo "Usage: $0 <link>"
+    echo ""
+    echo "<link> link to video/playlist/video youtube (most of these links, who you can pass to youtube-dl`"
+    exit 0
+fi
+
+set -x
+youtube-dl -i --write-description -v --no-check-certificate --write-sub --all-subs $@ 
